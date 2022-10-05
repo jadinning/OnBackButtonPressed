1948346c89c3296cc556b8aa35c0dceaf51315ce
John <John@JOHNSLENOVOP500>
2022-10-05 12:06:32 +1100

    Add project files.

---
+++ OnBackButtonPressed/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OnBackButtonPressed
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void DoSomething()
+        {
+            await DisplayAlert("Info", "OnBackButtonPressed", "OK");
+            await Application.Current.MainPage.Navigation.PopAsync(); //Close this Page (Pop the page off the top of the navigation stack)
+        }
+
+        //this overrides the hardware back button (not the navigation bar back button)
+        protected override bool OnBackButtonPressed()
+        {
+            base.OnBackButtonPressed();
+            DoSomething();
+            return true;
+        }
+    }
+}
